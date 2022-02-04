28ca04471f1259b6b43410f4551dc7a1807b55c8
Lucas <lucasazzollinivieira@gmail.com>
2022-02-03 23:51:07 -0300

    feat: db connection 'componentized'

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+await mongoClient.connect();
+
+const db = mongoClient.db("MyWallet_backEnd");
+export default db;
