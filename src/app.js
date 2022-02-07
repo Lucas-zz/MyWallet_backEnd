903744c8d45852abdb20e351806a45a5d9e11e47
Lucas <lucasazzollinivieira@gmail.com>
2022-02-07 15:11:29 -0300

    chore: adjust code for deploy on heroku

--- src/app.js
+++ src/app.js
@@ -5,13 +5,12 @@ import chalk from 'chalk';
 import router from './routes/index.js';
 
 const app = express();
-const port = 5000;
 
 app.use(cors());
 app.use(json());
 
 app.use(router);
 
-app.listen(port, () => {
-    console.log(`Servidor ${chalk.bgGreen(chalk.black(' ON '))} - Porta ${chalk.magenta(port)} - ${chalk.blue(`http://localhost:${port}`)}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor ${chalk.bgGreen(chalk.black(' ON '))} - Porta ${chalk.magenta(process.env.PORT)} - ${chalk.blue(`http://localhost:${process.env.PORT}`)}`);
 });
