b7dae422e2d52d3654e3911cebe06d700358b612
Lucas <lucasazzollinivieira@gmail.com>
2022-02-08 21:13:19 -0300

    chore: adjust code for deploy on heroku

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient("mongodb+srv://lucas:azzolini23@cluster0.fivmt.mongodb.net/mywallet-backend?retryWrites=true&w=majority");
 
 await mongoClient.connect();
 
-const db = mongoClient.db("MyWallet_backEnd");
+const db = mongoClient.db("mywallet-backend");
 export default db; 
